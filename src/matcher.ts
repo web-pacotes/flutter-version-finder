8d97c871a4ae7cd6e35bbd7950b48977d9533a44
freitzzz <1160907@isep.ipp.pt>
2023-05-30 21:08:23 +0100

    style: format

--- src/matcher.ts
+++ src/matcher.ts
@@ -1,16 +1,17 @@
-import supportedVersions, { FallbackVersion, Version } from "./version";
+import supportedVersions, { FallbackVersion, Version } from './version';
 
 export default function (dart: string): Version {
-    const version = dart;
+	const version = dart;
 
-    const supportedVersion = supportedVersions.find((x) => x.dart === version);
+	const supportedVersion = supportedVersions.find((x) => x.dart === version);
 
-    if (supportedVersion) {
-        return supportedVersion
-    };
+	if (supportedVersion) {
+		return supportedVersion;
+	}
 
-    const higherVersions = supportedVersions.filter((x) => x.dart > version);
+	const higherVersions = supportedVersions.filter((x) => x.dart > version);
 
-
-    return higherVersions.length > 0 ? higherVersions[higherVersions.length - 1] : FallbackVersion;
+	return higherVersions.length > 0
+		? higherVersions[higherVersions.length - 1]
+		: FallbackVersion;
 }
