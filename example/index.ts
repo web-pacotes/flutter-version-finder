ea1ec2330924da8b65b6ed2b5d240c47c3af7ce3
freitzzz <1160907@isep.ipp.pt>
2023-05-31 07:59:47 +0100

    feat: add example of finding the matching Flutter version for an example pubspec.lock file

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,17 @@
-import sum from 'flutter-version-finder';
+import { default as finder } from 'flutter-version-finder';
+import { readFileSync } from 'fs';
 
 function main() {
-	console.log(sum(40, 2));
+	// Read the content of a pubspec.yaml or pubspec-lock.yaml file
+	const path = 'example/example.lock';
+
+	const data = readFileSync(path).toString();
+
+	// Find the matching Flutter version
+	const result = finder(data);
+
+	// Hoooraaaay! It should print the Flutter version that is compatible with the lock file 
+	console.log(result);
 }
 
 main();
