80f1daac80a5ec6b4ddaf06906284eec10c5e083
freitzzz <1160907@isep.ipp.pt>
2023-06-10 12:25:25 +0100

    refactor: update handler to receive version channel

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 import matcher from './matcher';
 import parse from './parser';
-import { UnknownDartVersion, Version } from './version';
+import { Channel, UnknownDartVersion, Version } from './version';
 
 /**
  * Find the supported Flutter version for a pubspec.yaml/lock file.
@@ -8,8 +8,8 @@ import { UnknownDartVersion, Version } from './version';
  * @param yaml - A string describing the content of the pubspec file.
  * @returns The matched {@link Version} that is supported  for the project described by the pubspec file.
  */
-export default function (yaml: string): Version {
+export default function (yaml: string, channel?: Channel): Version {
 	const dartVersion = parse(yaml) ?? UnknownDartVersion;
 
-	return matcher(dartVersion);
+	return matcher(dartVersion, channel);
 }
