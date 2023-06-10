11b371e8909887167a04e27257f9202c9722f507
freitzzz <1160907@isep.ipp.pt>
2023-06-10 12:23:12 +0100

    refactor: update matcher to receive version channel

--- src/matcher.ts
+++ src/matcher.ts
@@ -1,23 +1,29 @@
-import supportedVersions, { FallbackVersion, Version } from './version';
+import supportedVersions, { Channel, FallbackBetaVersion, FallbackStableVersion, UnknownDartVersion, Version, versionToNumber } from './version';
 
 /**
  * Finds the supported {@link Version} for a specific Flutter project, by matching the supported Dart version.
  *
  * @param dart - A string that identifies the Dart version in the semantic versioning format
- * @returns The matched {@link Version} or as a fallback {@link FallbackVersion}
+ * @returns The matched {@link Version} or as a fallback {@link FallbackStableVersion}
  */
-export default function (dart: string): Version {
+export default function (dart: string, channel: Channel = 'stable'): Version {
 	const version = dart;
 
-	const supportedVersion = supportedVersions.find((x) => x.dart === version);
+	if (version === UnknownDartVersion) {
+		return FallbackStableVersion;
+	}
+
+	const dartVersionNumber = versionToNumber(dart);
+
+	const supportedVersion = supportedVersions.find((x) => x.dartNumber === dartVersionNumber && x.channel === channel);
 
 	if (supportedVersion) {
 		return supportedVersion;
 	}
 
-	const higherVersions = supportedVersions.filter((x) => x.dart > version);
+	const higherVersions = supportedVersions.filter((x) => x.dartNumber > dartVersionNumber && x.channel === channel);
 
 	return higherVersions.length > 0
 		? higherVersions[higherVersions.length - 1]
-		: FallbackVersion;
+		: (channel === 'stable' ? FallbackStableVersion : FallbackBetaVersion);
 }
